7f8bbfdb65589c13adca6276218d5d74dd20cafc
Awiwa <murilodebiazi@gmail.com>
2025-09-24 11:29:10 -0300

    Aula 3
    Estacionamento e vagas

---
+++ naoseioqueehprafazer/src/Estacionamento.java
@@ -0,0 +1,49 @@
+public class Estacionamento {
+    private Vaga[][] vagas;
+
+    public Estacionamento(Vaga[][] vagas){
+        this.vagas = vagas;
+    }
+
+    public Vaga[][] getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Vaga[][] vagas) {
+        this.vagas = vagas;
+    }
+
+    public Vaga Estacionar(String placa){
+        for(int i=0; i < this.vagas.length; i++) {
+            for (int j = 0; j < this.vagas[i].length; j++) {
+                if (this.vagas[i][j].getPlaca() == null) {
+                    vagas[i][j].setPlaca(placa);
+                    return vagas[i][j];
+                }
+            }
+        }
+        return null;
+    }
+
+    public void listarVagas(){
+        System.out.println("Vagas:");
+        for(int i=0; i < this.vagas.length; i++) {
+            for (int j = 0; j < this.vagas[i].length; j++) {
+                System.out.println(this.vagas[i][j]);
+            }
+        }
+    }
+
+    public boolean liberarVaga(String placa){
+        for(int i=0; i < this.vagas.length; i++) {
+            for (int j = 0; j < this.vagas[i].length; j++) {
+                if (this.vagas[i][j].getPlaca().equals(placa)) {
+                    this.vagas[i][j] = null;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
---
+++ naoseioqueehprafazer/src/Vaga.java
@@ -0,0 +1,44 @@
+public class Vaga {
+    private int andar;
+    private int pessoas;
+    private boolean coberta;
+    private String placa;
+
+    public Vaga(int andar, int pessoas, boolean coberta){
+        this.andar = andar;
+        this.pessoas = pessoas;
+        this.coberta = coberta;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void setAndar(int andar) {
+        this.andar = andar;
+    }
+
+    public int getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(int pessoas) {
+        this.pessoas = pessoas;
+    }
+
+    public boolean getCoberta() {
+        return coberta;
+    }
+
+    public void setCoberta(boolean coberta) {
+        this.coberta = coberta;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
